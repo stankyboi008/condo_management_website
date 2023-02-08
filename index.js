dcf8ea635018bac0b8c9f6aa6ed4077dd7e9380d
stankyboi008 <neeraj.kumbar.2019@vjc.edu.sg>
2023-02-08 10:02:27 +0800

    Installed and initalised wax-on, installed mongodb client, created the public folder for static files and created views folder+layouts folder for template. Created the base.hbs file which other files can inherit from

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //Importing express for use in nodejs
 const express = require('express');
+const wax = require('wax-on');
 
 const app = express();
 
 //intialise the view engine
 app.set('view engine','hbs');
 
+//intialise express to use static files
+//telling express where to find these static files
+app.use(express.static('./public'));
+
+//Initalise wax-on
+wax.on(hbs.handlebars);
+//Where the template for inheritance is located
+wax.setLayoutPath('./views/layouts');
 
 app.get('/',function(req,res) {
   res.send("Hello World!");
